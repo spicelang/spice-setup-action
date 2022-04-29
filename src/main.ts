579169968824801a29569c0bc6d09857980b5f82
Marc Auberer <marc.auberer@chillibits.com>
2022-04-29 17:32:24 +0200

    Bump latest version to 0.8

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import path from 'path';
 import cp from 'child_process';
 import {URL} from 'url';
 
-const defaultVersion: string = '0.7';
+const defaultVersion: string = '0.8';
 
 export async function run() {
   try {
